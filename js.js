256496c4a95a4aeaa15c4c2464a0536e78c62031
kekevishnya <126613207+kekevishnya@users.noreply.github.com>
2023-04-05 02:31:00 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -7,27 +7,29 @@ function pay() {
                amount: 100, //сумма
                currency: 'RUB', //валюта
                accountId: 'user@example.com', //идентификатор плательщика (необязательно)
+               InvoiceId: '1234567', 
                email: 'user@example.com', //email плательщика (необязательно)
                skin: "mini", //дизайн виджета (необязательно)
-               autoClose: 3, //время в секундах до авто-закрытия виджета (необязательный)
+               autoClose: 10, //время в секундах до авто-закрытия виджета (необязательный)
                data: {
                    myProp: 'myProp valuße'
                },
                payer: { 
-                   firstName: document.getElementById("name_field").textContent,
-                   lastName: document.getElementById("last_name_field").textContent,
-                   middleName: document.getElementById("middle_name_field").textContent,
-                   birth: document.getElementById("datebirth").textContent,
-                   address: Object.payer().,
-                   street: 'Lenina',
-                   city: 'MO',
-                   country: 'RU',
-                   phone: '123',
-                   postcode: '345'
+                firstName: 'Тест',
+                lastName: 'Тестов',
+                middleName: 'Тестович',
+                birth: '1955-02-24',
+                address: 'тестовый проезд дом тест',
+                street: 'Lenina',
+                city: 'MO',
+                country: 'RU',
+                phone: '123',
+                postcode: '345'
                }
            },
            {
-               onSuccess: function (options) { // success
+               onSuccess: function (options) {
+              success
                    //действие при успешной оплате
                },
                onFail: function (reason, options) { // fail
